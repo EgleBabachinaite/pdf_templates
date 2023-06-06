cbe741cd8fc2a85122ef50af7ceaa0e3b0be7e5b
Predator <eglebabachinaite123@gmail.com>
2023-06-06 14:03:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    # setting a font
+    pdf.set_font(family="Times", style="B", size=24)
+    # adding rgb values for the gray color
+    pdf.set_text_color(100, 100, 100)
+    # adding a text through cells
+    pdf.cell(w=0, h=12, txt=row["Topic"],
+             align="L", ln=1)
+    # coordinates as the arguments
+    pdf.line(10, 22, 200, 22)
+
+
+pdf.output("output.pdf")
